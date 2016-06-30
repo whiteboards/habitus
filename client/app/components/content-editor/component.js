6897acd24ff92ec1780619be56dcbcd60e0e9d89
Nathan Bland <nathan.bland@gmail.com>
2016-06-30 15:51:31 -0600

    editor kind of sort of working. Save does not happen yet though

--- client/app/components/content-editor/component.js
+++ client/app/components/content-editor/component.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
+import marked from 'npm:marked'
 
 export default Ember.Component.extend({
+  content: '',
+  title: '',
+  previewContent: Ember.computed('content', function () {
+    return marked(this.get('content'))
+  }) 
 });
