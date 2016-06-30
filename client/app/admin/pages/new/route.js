c3d05bc011a9a6d6a4886697283ce9a9b8dfeb6f
Nathan Bland <nathan.bland@gmail.com>
2016-06-30 16:07:56 -0600

    started work on save but it is over my head at the moment

--- client/app/admin/pages/new/route.js
+++ client/app/admin/pages/new/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    savePage() {
+      let page = store.createRecord('page', {
+        content: this.get('content'),
+        title: this.get('title')
+      })
+      page.save().then(function (){
+        this.transitionTo('admin.pages');
+      })
+    }
+  }
 });
--- client/app/components/content-editor/component.js
+++ client/app/components/content-editor/component.js
@@ -6,5 +6,14 @@ export default Ember.Component.extend({
   title: '',
   previewContent: Ember.computed('content', function () {
     return marked(this.get('content'))
-  }) 
+  }), 
+  alias: Ember.computed('title', function () {
+    return this.get('title').replace(/[^a-zA-Z0-9-_]/g, '-').toLowerCase()
+  }),
+  actions: {
+    submitPage() {
+      // trigger action on parent component
+      this.set('confirmShown', false);
+    },
+  } 
 });
