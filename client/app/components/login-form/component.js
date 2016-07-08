d231f9499e979ee459562e3d38f8c4b2b3b807f5
Nathan Bland <nathan.bland@gmail.com>
2016-07-08 08:19:55 -0600

    added authorizer

--- client/app/components/login-form/component.js
+++ client/app/components/login-form/component.js
@@ -6,8 +6,6 @@ export default Ember.Component.extend({
     authenticate() {
       let displayName = this.get('displayName');
       let password = this.get('password');
-      console.log('display_name', displayName)
-      console.log('password', password)
       this.get('session').authenticate('authenticator:oauth2', displayName, password).catch((reason) => {
         this.set('errorMessage', reason.error || reason);
       });
